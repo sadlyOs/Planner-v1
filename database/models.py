d33843a05e6d88d153a7334334acc8dd6a53d3d3
sadlyOs <jukovkill@gmail.com>
2024-04-22 16:14:48 +0500

    create Task modele

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Boolean, Column, BigInteger, DateTime, ForeignKey, Integer, String
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -8,6 +8,15 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(BigInteger, primary_key=True)
     lang = Column(String(2), default='ru')
-    
+    tasks = relationship("Task", back_populates='user')
 
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    title = Column(String)
+    description = Column(String)
+    due_datetime = Column(DateTime)
+    completed = Column(Boolean, default=False)
+    user = relationship('User', back_populates="tasks")
     
